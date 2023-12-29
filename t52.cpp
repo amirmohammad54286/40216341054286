3caaeee4f786794b6056e1dbc37b723d7b82fd7e
amirmohammad54286 <155174484+amirmohammad54286@users.noreply.github.com>
2023-12-29 21:04:55 +0330

    Add files via upload

---
+++ t52.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+
+for(int i=100; i>0 ; i=i-2)
+{
+cout<<i<<" ";
+}
+getch();
+}
---
+++ t8.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main(){
+
+long int s=0,a;
+
+cout<<"enter your numbers : "<<endl;
+cin>>a;
+
+while (a!=0){
+s+=a;
+cin>>a;
+}
+cout<<"Sum is "<<s;
+}
